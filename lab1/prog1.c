e1604b64a660c3d27be2dc1179155b08af256e0d
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2021-10-19 15:02:11 -0400

    updated prog1 code to work in OpenSSL 1.1.1 (breaking changes without backward compatibility were introduced in 2018 in 1.1.0), commented out line 22 bc unnecessary/mildly confusing

--- lab1/prog1.c
+++ lab1/prog1.c
@@ -8,25 +8,25 @@ int main() {
 	unsigned char key[EVP_MAX_KEY_LENGTH+1] = "This is a key";
 	unsigned char iv[EVP_MAX_IV_LENGTH+1] = "My iv";
 	int in_len, msg_size, out_len = 0;
-	EVP_CIPHER_CTX ctx;
+	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
 
 	msg_size = in_len = strlen((char *)plaintext);
 	printf("Plaintext = %s\n", plaintext);
 
-	EVP_EncryptInit(&ctx, EVP_rc4(), key, iv);
-	EVP_EncryptUpdate(&ctx, ciphertext, &out_len, plaintext, in_len);
-	EVP_EncryptFinal(&ctx, &ciphertext[out_len], &out_len);
+	EVP_EncryptInit(ctx, EVP_rc4(), key, iv);
+	EVP_EncryptUpdate(ctx, ciphertext, &out_len, plaintext, in_len);
+	EVP_EncryptFinal(ctx, &ciphertext[out_len], &out_len);
 	printf("Ciphertext = %s\n", ciphertext);
 
 	memset(plaintext, 0, msg_size);
-	printf("Plaintext = %s\n", plaintext);
+//	printf("Plaintext = %s\n", plaintext);
 
-	EVP_DecryptInit(&ctx, EVP_rc4(), key, iv);
-	EVP_DecryptUpdate(&ctx, plaintext, &in_len, ciphertext, msg_size);
-	EVP_DecryptFinal(&ctx, &plaintext[in_len], &in_len);
+	EVP_DecryptInit(ctx, EVP_rc4(), key, iv);
+	EVP_DecryptUpdate(ctx, plaintext, &in_len, ciphertext, msg_size);
+	EVP_DecryptFinal(ctx, &plaintext[in_len], &in_len);
 	printf("Decrypted plaintext = %s\n", plaintext);
 
-	EVP_CIPHER_CTX_cleanup(&ctx);
+	EVP_CIPHER_CTX_cleanup(ctx);
 	return 0;
 }
 
