6215fe81a29323b6557b5706415e2f552d14ba1e
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2021-11-23 22:12:05 -0500

    added key length variation

--- lab2/runtime.sh
+++ lab2/runtime.sh
@@ -8,19 +8,24 @@ do
 done
 
 # set up output file
-echo "Algorithm, Input, real, user, sys" > log.csv
+echo "Algorithm, Msg size, Key size, real, user, sys" > log.csv
 
 # algorithms=( "-aes-128-cbc" "-aes-256-cbc" "-des-ede-cbc" )
 algorithms=()
 while read line; do algorithms+=($line); done < algorithms.txt
+digests=()
+while read line; do digests+=($line); done < digests.txt
 
-for size in "${sizes[@]}"
+for msg_size in "${sizes[@]}"
 do
 	for alg in "${algorithms[@]}"
 	do
-		echo "ahh $alg"
+		for key_size in "${sizes[@]}"
+		do
+			echo "$alg msg:$msg_size key:$key_size"
 		# time openssl enc -e $alg -k "This's my key" -in $size.txt -out ciphertext.bin
-		/usr/bin/time -ao log.csv -f "$alg, $size, %e, %U, %S" openssl enc -e -$alg -k "This's my key" -in $size.txt -out ciphertext.bin >> log.csv
+		/usr/bin/time -ao log.csv -f "$alg, $msg_size, $key_size, %e, %U, %S" openssl enc -e -$alg -k $key_size.txt -in $msg_size.txt -out ciphertext.bin >> log.csv
+		done
 	done
 done
 
