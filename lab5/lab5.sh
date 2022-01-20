e8f816b128f014d703ff073e4384e95fb9c9c536
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2022-01-20 13:05:48 -0500

    finished lab5 bash script

--- lab5/lab5.sh
+++ lab5/lab5.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-read -p "To run the next command hit ENTER"
+read -p "To run each command for Lab 5, hit ENTER"
+
+OLDIFS="$IFS"
+IFS=$'\n'
 
 commands=()
 while IFS= read -r line; do commands+=($line); done < commands.txt
@@ -8,5 +11,7 @@ while IFS= read -r line; do commands+=($line); done < commands.txt
 for command in "${commands[@]}"
 do
 	read -p "$ $command"
-	$command
+	eval "$command"
 done
+IFS="$OLDIFS"
+
